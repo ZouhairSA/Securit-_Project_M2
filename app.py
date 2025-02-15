6dac883e2a94fd408f04b25dd9a145b8fdb4ba43
Sabyoud Zohair <109246617+ZouhairSA@users.noreply.github.com>
2025-02-15 09:48:28 +0100

    try to deploy

--- app.py
+++ app.py
@@ -178,5 +178,8 @@ def logout():
     # Rediriger vers la page de connexion
     return redirect(url_for('login'))
 
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
